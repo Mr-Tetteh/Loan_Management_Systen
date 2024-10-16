c3f78dfbc062f407b7e8913b4314886a6f7eecae
Mr. Tetteh <danielstay73@gmail.com>
2024-10-16 20:57:00 +0000

    forgot password backend compeleed

--- Server/app/Http/Controllers/PasswordController.php
+++ Server/app/Http/Controllers/PasswordController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
@@ -13,18 +15,41 @@ class PasswordController extends Controller
 {
     public function forgotPassword(Request $request)
     {
-      $email = $request->input('email');
-      $token = Str::random(20);
+        $email = $request->input('email');
+        $token = Str::random(20);
 
-      DB::table('password_reset')->insert([
-          'email' => $email,
-          'token' => $token,
-          'created_at' => Carbon::now()
-      ]);
-      Mail::send('Rest', ['token' => $token], function (Message $message) use ($email) {
-          $message->subject('Reset your Password');
-          $message->to($email);
-      });
-      return response()->json(['message' => 'Reset password link sent on your email.']);
+        DB::table('password_reset')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+        Mail::send('Rest', ['token' => $token], function (Message $message) use ($email) {
+            $message->subject('Reset your Password');
+            $message->to($email);
+        });
+        return response()->json(['message' => 'Reset password link sent on your email.']);
+    }
+
+
+    public function resetPassword(Request $request)
+    {
+        if ($request->input('password') !== $request->input('confirm_password')) {
+            return response()->json(['message' => 'Passwords not match.'], 400);
+        }
+        $passwordReset = DB::table('password_reset')->where('token', $request->input('token'))->first();
+
+        $user = User::where('email', $passwordReset->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Sorry User not found'
+            ], 404);
+        }
+
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        return response()->json([
+            'message' => 'Password reset successfully.'
+        ]);
     }
 }
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoanController;
+use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\PaymentsController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\Cors;
@@ -10,9 +11,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
-// rest password
 
-Route::post('reset ', [\App\Http\Controllers\PasswordController::class, 'forgotPassword']);
+// rest password
+Route::post('forgot ', [PasswordController::class, 'forgotPassword']);
+Route::post('reset ', [PasswordController::class, 'resetPassword']);
+
+
 
 Route::middleware('auth:sanctum')->group(function () {
     //Auth

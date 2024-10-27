0c2464086bd5688620803e9b69a92764323ec435
Mr. Tetteh <danielstay73@gmail.com>
2024-10-27 09:54:08 +0000

    remove vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -10,15 +10,7 @@ export default defineConfig({
         vue(),
         vueDevTools(),
     ],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            }
-        }
-    },
+
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
